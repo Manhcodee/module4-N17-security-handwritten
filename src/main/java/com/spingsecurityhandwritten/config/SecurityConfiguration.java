93f5f511525be3ed12939d8bc48edadbfe118696
Manhcodee <phammanh2952@gmail.com>
2025-02-04 21:56:38 +0700

    done

--- src/main/java/com/spingsecurityhandwritten/config/SecurityConfiguration.java
+++ src/main/java/com/spingsecurityhandwritten/config/SecurityConfiguration.java
@@ -1,27 +1,21 @@
 package com.spingsecurityhandwritten.config;
 
-import com.spingsecurityhandwritten.controller.CustomSuccessHandle;
-import org.apache.catalina.realm.AuthenticatedUserRealm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 
 
 import javax.sql.DataSource;
