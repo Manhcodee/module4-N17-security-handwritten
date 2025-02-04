aec170be56ac168f00d2da6405f363a65af959f2
Manhcodee <phammanh2952@gmail.com>
2025-02-04 17:34:49 +0700

    go home

--- src/main/java/com/spingsecurityhandwritten/controller/CustomSuccessHandle.java
+++ src/main/java/com/spingsecurityhandwritten/controller/CustomSuccessHandle.java
@@ -1,8 +1,70 @@
 package com.spingsecurityhandwritten.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class CustomSuccessHandle {
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+@Controller
+public class CustomSuccessHandle extends SimpleUrlAuthenticationSuccessHandler {
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String targetUrl = daterminTargetUrl(request, request, authentication);
+        if (response.isCommitted()) {
+            return;
+        }
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String daterminTargetUrl(Authentication authentication) {
+        String url;
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = new ArrayList<>();
+        for (GrantedAuthority a : authorities) {
+            roles.add(a.getAuthority());
+        }
+        if (isDba(roles)) {
+            url = "/dba";
+        }else if (isAdmin(roles)) {
+            url = "/admin";
+        } else if (isUser(roles)) {
+            url = "/user";
+        } else {
+            url = "/accessDenied";
+        }
+        return url;
+    }
+
+    private boolean isUser(List<String> roles) {
+        return roles.contains("USER");
+    }
+
+    private boolean isAdmin(List<String> roles) {
+        return roles.contains("ADMIN");
+    }
+
+    private boolean isDba(List<String> roles) {
+        return roles.contains("DBA");
+    }
+
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+
+    public RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
 }
