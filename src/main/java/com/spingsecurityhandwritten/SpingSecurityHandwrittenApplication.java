713c039b2cfc24c98bc429441b7a30465ef6ec56
Manhcodee <phammanh2952@gmail.com>
2025-02-04 16:20:45 +0700

    first commit

---
+++ src/main/java/com/spingsecurityhandwritten/SpingSecurityHandwrittenApplication.java
@@ -0,0 +1,13 @@
+package com.spingsecurityhandwritten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSecurityHandwrittenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingSecurityHandwrittenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spingsecurityhandwritten/SpingSecurityHandwrittenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingsecurityhandwritten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingSecurityHandwrittenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
